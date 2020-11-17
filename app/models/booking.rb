c9fd40d5d99617b6ac24f6f50260c5a98fb876bb
Alain Rusnov <alainrusnov@gmail.com>
2020-11-16 21:45:27 -0500

    added initial validations to all 3 models - needs review

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,9 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :dog
+
+  validates :description, presence: true
+  validates :user, presence: true, uniqueness: { scope: :dog }
+  validates :dog, presence: true
+  validates :status, presence: true
 end
