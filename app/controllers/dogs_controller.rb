2e9aff3444141c22d8070f5528588ced11ba405b
Alain Rusnov <alainrusnov@gmail.com>
2020-11-16 22:24:58 -0500

    defined actions in DogsController

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,41 @@
 class DogsController < ApplicationController
+  skip_before_action :authenticate_user!, only:[:index, :show]
+  before_action :set_dog, only:[:show, :destroy]
+
+  def index
+  @dogs = Dog.all
+  end
+
+  def show
+  end
+
+  def new
+    @dog = Dog.new
+  end
+
+  def create
+    @dog = Dog.new(dog_params)
+    if @dog.save
+      redirect_to dog_path(@dog)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dog.destroy
+    redirect_to dogs_path(@dog)
+  end
+
+  private
+
+  def set_dog
+    @dog = Dog.find(params[:id])
+  end
+
+  def dog_params
+    params.require(:dog).permit(:name, :breed, :description, :user_id)
+  end
+
 end
+
