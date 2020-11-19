7708873a19f6353813db389ebe6f6968586a85a9
Alejandro Calzadilla <alejandrocalzadilla@gmail.com>
2020-11-19 13:13:38 -0500

    Seeds reworked, added 10 real addresses in MTL

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,22 +22,25 @@ adam = User.create(
 dog_attributes = [
   { address: "5333 Avenue Casgrain, Montreal" },
   { address: "1145 Avenue Lajoie, Outremont" },
+  { address: "7503 Quebec Highway 335, Montreal"},
+  { address: "251 Av Percival, Montreal West"},
+  { address: "7766 Rue George, Lachine"},
+  { address: "11727 Rue Notre Dame E, Montreal"},
+  { address: "1017 avenue des erables, Montreal"},
+  { address: "3708 Rue Saint-Hubert, Montreal"},
+  { address: "800 Rue Gagné, Lachine"},
+  { address: "4430 Rue Sainte-Catherine Ouest, Westmount"}
 ]
 dog_attributes.each do |dog|
   dog = Dog.create(
     name: Faker::Creature::Dog.name,
     breed: Faker::Creature::Dog.breed,
-    address: dog[:address] ,
-
-
-# 10.times do
-#   dog = Dog.create(
-#     name: Faker::Creature::Dog.name,
-#     breed: Faker::Creature::Dog.breed,
-#     address: Faker::Address.full_address,
-#     age: rand(1..12),
-#     size: %w[small medium large ].sample,
-#     description: Faker::Creature::Dog.meme_phrase,
-#     user: adam
-#   )
+    address: dog[:address],
+    age: rand(1..12),
+    size: %w[small medium large ].sample,
+    description: Faker::Creature::Dog.meme_phrase,
+    user: adam
+  )
 end
+
+puts "Done!"
