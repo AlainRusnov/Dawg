d9963f83059b7332ef3507e30779f5861708f6b0
Alejandro Calzadilla <alejandrocalzadilla@gmail.com>
2020-11-19 16:51:46 -0500

    Search by size and breed done

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -6,6 +6,15 @@ class Dog < ApplicationRecord
   # HAS MANY USERS THROUGH BOOKINGS ??? // Dont think so but asking in case
   has_many :bookings, dependent: :destroy
   has_many :renters, through: :bookings, source: :user
+
+  # pg search method
+  include PgSearch::Model
+  pg_search_scope :search_by_size_and_breed,
+    against: [ :size, :breed ],
+    using: {
+      tsearch: { prefix: true }
+    }
+
   validates :name, presence: true
   validates :breed, presence: true
   validates :description, presence: true
